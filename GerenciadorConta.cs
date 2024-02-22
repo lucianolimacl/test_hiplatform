f1907b63798417ae9f80d5029b7904afd35d45f8
luciano cardoso lima <luciano.cl@hotmail.com>
2024-02-21 21:13:09 -0300

    teste

---
+++ GerenciadorConta.cs
@@ -0,0 +1,57 @@
+﻿namespace Test_hiplatform
+{
+    using System.Transactions;
+    public class GerenciadorConta
+    {
+        private readonly ContaDao contaDao;
+
+        public void Debitar(long idConta, double valor)
+        {
+            using (TransactionScope tran = new TransactionScope())
+            {
+                Conta conta = contaDao.BuscaConta(idConta);
+                if (conta.PodeDebitar(valor))
+                {
+                    conta.Debite(valor);
+                    contaDao.Atualiza(conta);
+                    tran.Complete();
+                }
+                else
+                {
+                    tran.Dispose();
+                    throw new SaldoInsuficienteException();
+                }
+            }
+        }
+
+        public void creditar(long idConta, double valor)
+        {
+            using (TransactionScope tran = new TransactionScope())
+            {
+                Conta conta = contaDao.BuscaConta(idConta);
+                conta.Credite(valor);
+                contaDao.Atualiza(conta);
+                tran.Complete();
+            }
+        }
+
+
+
+        #region Classes
+        abstract class Conta
+        {
+            public abstract bool PodeDebitar(double valor);
+            public abstract void Debite(double valor);
+            public abstract void Credite(double valor);
+        }
+
+        interface ContaDao
+        {
+            Conta BuscaConta(long idConta);
+            void Atualiza(Conta conta);
+        }
+
+        public class SaldoInsuficienteException : Exception { }
+        #endregion
+    }
+}
